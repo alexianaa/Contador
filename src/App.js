7eeb6e46cbf77700956ffe4c3adf260b58bb393d
alexianaa <202045007@aluno.unb.br>
2022-07-11 23:24:49 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const myElement = <h1>I Love JSX!</h1>;
+  const tabela = <h2>{count}</h2>
+  const button = <button onClick={() => setCount(count+1)}>Cresce um</button>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="caixa">
+      <div className="box">
+        {myElement}
+        {tabela}
+        {button}
+      </div>
     </div>
+   
   );
 }
 
